c25cbfc36be954b736ebec3de6753e69424bc4da
aabdelw1 <ammar.s.nasr@gmail.com>
2023-03-08 11:24:04 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Routes,
+  Route,
+  Outlet,
+  Link,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
 
-function App() {
+const AppContext = React.createContext({});
+
+const ContextProvider = (props) => {
+  const [allItems, setAllItems] = React.useState([]);
+
+  return (
+    <AppContext.Provider value={{ allItems, setAllItems }}>
+      {props.children}
+    </AppContext.Provider>
+  );
+};
+
+const products = [
+  {
+    name: "nissan",
+    price: "10",
+    id: 1,
+    image: require("../src/static/Automotive.png"),
+    colors: ["blue", "orange", "green"],
+    doors: ["two", "four"],
+  },
+  {
+    name: "Home",
+    price: "100",
+    id: 2,
+    image: require("../src/static/Home.png"),
+    colors: ["blue", "orange", "green"],
+    doors: ["two", "four"],
+  },
+];
+
+function Home() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {products.map((item) => {
+        return <Card item={item} key={item.id} />;
+      })}
     </div>
   );
 }
+
+const Card = (props) => {
+  const { id, name, price, image } = props.item;
+  return (
+    <Link to={`product/${name}`}>
+      <div key={id}>
+        <div>{name}</div>
+        <div>{price}</div>
+        <img src={image} />
+      </div>
+    </Link>
+  );
+};
+
+const Product = () => {
+  let { name } = useParams();
+  let [searchParams] = useSearchParams();
+  const { allItems, setAllItems } = useContext(AppContext);
+
+  const [item] = products.filter((item) => item.name === name);
+
+  const color = searchParams.get("color") || item.colors[0];
+  const door = searchParams.get("door") || item.doors[0];
+  console.log(allItems);
+
+  const toogleAdd = () => {
+    const myItem = {
+      name: item.name,
+      color,
+      door,
+    };
+    setAllItems([...allItems, myItem]);
+  };
+
+  return (
+    <div>
+      <h1>
+        {item.name} {item.price}
+      </h1>
+      <h3>
+        {color} {door}
+      </h3>
+      <h2>Colors</h2>
+      {item.colors.map((color) => {
+        return <Link to={`?color=${color}&door=${door}`}>{color} </Link>;
+      })}
+      <h2>Doors</h2>
+      {item.doors.map((door) => {
+        return <Link to={`?color=${color}&door=${door}`}>{door} </Link>;
+      })}
+      <button onClick={() => toogleAdd()}>Add To Cart</button>
+    </div>
+  );
+};
+
+const Cart = () => {
+  const { allItems } = useContext(AppContext);
+  console.log(allItems)
+  return (
+    <div>
+      {allItems &&
+        allItems.map((item) => {
+          return (
+            <div>
+              <h1>{item.name}</h1>
+              <h2>{item.color}</h2>
+              <h3>{item.door}</h3>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+const App = () => {
+  return (
+    <ContextProvider>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="product/:name" element={<Product />} />
+        <Route path="cart" element={<Cart />} />
+      </Routes>
+    </ContextProvider>
+  );
+};
 
 export default App;
